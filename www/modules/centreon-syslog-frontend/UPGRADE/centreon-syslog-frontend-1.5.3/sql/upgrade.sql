949cdbd98fc23f01a15a1e0a22aed70dff90f5fa
Laurent Pinsivy <lpinsivy@centreon.com>
2015-12-16 13:46:51 +0000

    Prepare v1.5.3 for compatibility with Centreon web 2.7

---
+++ www/modules/centreon-syslog-frontend/UPGRADE/centreon-syslog-frontend-1.5.3/sql/upgrade.sql
@@ -0,0 +1,13 @@
+UPDATE topology SET topology_page = '240' WHERE topology_page = '204' AND topology_name = 'Syslog';
+UPDATE topology SET topology_parent = '240', topology_page = '24001' WHERE topology_page = '20401' AND topology_name = 'Monitoring';
+
+DELETE FROM topology_JS WHERE id_page = '20402' AND PathName_js = './modules/centreon-syslog-frontend/include/search/javascript/exportCSV.js';
+DELETE FROM topology_JS WHERE id_page = '20402' AND PathName_js = './include/common/javascript/datePicker.js';
+DELETE FROM topology_JS WHERE id_page = '20402' AND PathName_js = './include/common/javascript/tool.js';
+
+
+UPDATE topology SET topology_parent = '240', topology_page = '24002' WHERE topology_page = '20402' AND topology_name = 'Search';
+
+INSERT INTO topology_JS (id_page, PathName_js) VALUES ('24002', './modules/centreon-syslog-frontend/include/search/javascript/exportCSV.js'),('24002', './include/common/javascript/datePicker.js'),('24002', './include/common/javascript/tool.js');
+
+UPDATE topology SET topology_parent = '240', topology_page = '24003' WHERE topology_page = '20403' AND topology_name = 'Details';
