d4e1a1d94434f0fd9fb5389328bfae679484065b
Laurent Pinsivy <lpinsivy@centreon.com>
2015-12-21 09:02:46 +0000

    Modify SQL uninstall requests

--- www/modules/centreon-syslog-frontend/sql/uninstall.sql
+++ www/modules/centreon-syslog-frontend/sql/uninstall.sql
@@ -3,11 +3,11 @@ DROP TABLE IF EXISTS `mod_syslog_collector`;
 DROP TABLE IF EXISTS `mod_syslog_filters_priority`;
 DROP TABLE IF EXISTS `mod_syslog_filters_facility`;
 
-DELETE FROM `topology` WHERE `topology_parent` = '204' AND `topology_name` = "Syslog";
-DELETE FROM `topology` WHERE `topology_page` = '20401' AND `topology_name` = "Monitoring";
-DELETE FROM `topology` WHERE `topology_page` = '20402' AND `topology_name` = "Search";
-DELETE FROM `topology` WHERE `topology_page` = '20403' AND `topology_name` = "Details";
-DELETE FROM `topology` WHERE `topology_page` = '204' AND `topology_name` = "Syslog";
+DELETE FROM `topology` WHERE `topology_parent` = '240' AND `topology_name` = "Syslog";
+DELETE FROM `topology` WHERE `topology_page` = '24001' AND `topology_name` = "Monitoring";
+DELETE FROM `topology` WHERE `topology_page` = '24002' AND `topology_name` = "Search";
+DELETE FROM `topology` WHERE `topology_page` = '24003' AND `topology_name` = "Details";
+DELETE FROM `topology` WHERE `topology_page` = '240' AND `topology_name` = "Syslog";
 DELETE FROM `topology` WHERE `topology_parent` = '605' AND `topology_name` = "Syslog";
 DELETE FROM `topology` WHERE `topology_parent` = '605' AND `topology_name` = "Collectors";
 DELETE FROM `topology` WHERE `topology_page` = '60502' AND `topology_name` = "Collectors";
@@ -15,6 +15,6 @@ DELETE FROM `topology` WHERE `topology_parent` = '605' AND `topology_name` = "Ge
 DELETE FROM `topology` WHERE `topology_page` = '60503' AND `topology_name` = "Resfresh";
 DELETE FROM `topology` WHERE `topology_page` = '605' AND `topology_name` = "Syslog";
 
-DELETE FROM `topology_JS` WHERE `id_page` = '204' LIMIT 1;
-DELETE FROM `topology_JS` WHERE `id_page` = '20401' LIMIT 1;
-DELETE FROM `topology_JS` WHERE `id_page` = '20402' LIMIT 1;
+DELETE FROM `topology_JS` WHERE `id_page` = '240' LIMIT 1;
+DELETE FROM `topology_JS` WHERE `id_page` = '24001' LIMIT 1;
+DELETE FROM `topology_JS` WHERE `id_page` = '24002' LIMIT 1;
